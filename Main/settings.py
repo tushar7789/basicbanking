dfc2b357f2dbe8293287a4c93fc306b3d181ec9f
tushar7789 <tusharjangid7@gmail.com>
2021-09-15 01:10:19 +0530

    modified Profile

--- Main/settings.py
+++ Main/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'django-insecure-#c5vz@o!u%m9op-*bgrlp%b^16e^52y04+m$na+h2c*8)hj^nj'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --listen=127.0.0.1:8000 Main.wsgi:application
+web: waitress-serve --port=$PORT Main.wsgi:application
